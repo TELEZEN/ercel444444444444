0ec7cb0f98169e922e8ec531919fb3ca4317293f
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-01-28 09:18:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -411,8 +411,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Arjun_La_Lis_A')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
+            await message.reply_photo(
+            photo=random.choice(PICS2),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
@@ -456,10 +457,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Arjun_La_Lis_A')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.ABOUT_TXT.format(temp.B_NAME)
-           message.reply_photo(
-            photo=random.choice(PICS3),
+            await message.reply_photo(
+            photo=random.choice(PICS2),
+            caption=script.ABOUT_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
